68f0781e24022c48c3e0d12bab4598f894a952fb
Lany <wlany@qq.com>
2019-09-26 13:03:20 +0800

    显示个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,10 +9,10 @@
                 <img class="card-img-top" src="{{config('app.default_avatar')}}">
                 <div class="card-body">
                     <h5><strong>个人简介</strong></h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <p>{{$user->introduction}}</p>
                     <hr>
                     <h5><strong>注册于</strong></h5>
-                    <p>January 01 1901</p>
+                    <p>{{ $user->created_at->diffForHumans() }}</p>
                 </div>
             </div>
         </div>

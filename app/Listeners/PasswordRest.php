a5c40e6496c96fb6cd87873d7674e61ea196c87e
Lany <wlany@qq.com>
2019-09-26 11:40:41 +0800

    找回密码成功消息提示

---
+++ app/Listeners/PasswordRest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class PasswordRest
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  PasswordReset  $event
+     * @return void
+     */
+    public function handle(PasswordReset $event)
+    {
+        //
+        session()->flash('success', '密码更新成功，您已成功登录！');
+    }
+}

069332f0d050bc339eb92600494dcd41488be668
Lany <wlany@qq.com>
2019-09-27 22:49:30 +0800

    消息通知

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 class ReplyObserver
 {
@@ -11,6 +12,7 @@ class ReplyObserver
         //$reply->topic->increment('reply_count',1);
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function creating(Reply $reply) {

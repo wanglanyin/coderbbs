42c4f3ed4e64b21b1a7f2d8a455337d145624bd8
Lany <wlany@qq.com>
2019-09-27 16:57:32 +0800

    翻译 URL 标示

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Handlers\SlugTranslateHandler;
 use App\Models\Topic;
 
 class TopicObserver
@@ -9,6 +10,12 @@ class TopicObserver
     public function saving(Topic $topic) {
         $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+            $trans = app(SlugTranslateHandler::class)->translate($topic->title);
+            $topic->slug = $trans;
+        }
     }
 
 
